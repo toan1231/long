374af9545874dcd4694466be00ff58da9b27644b
Chau Thai <chauthai2003@gmail.com>
2025-08-08 19:27:38 -0400

    hi

--- src/test/java/api_automation/TestingField/GoreTesting2.java
+++ src/test/java/api_automation/TestingField/GoreTesting2.java
@@ -4,6 +4,7 @@ package api_automation.TestingField;
 import api_automation.dataProviderClass.GorestDataProviderClass;
 import api_automation.utils.TestBase;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GoreTesting2 extends TestBase {
@@ -11,7 +12,7 @@ public class GoreTesting2 extends TestBase {
     @Test()
     public void user_create_requestData()  {
         System.out.println("class2");
-
+        Assert.assertEquals(1,2);
 
     }
 }
